d6a851855826a826dcdc655723fb352f5fdd58ee
J-Skarbek <john@snarkmediaagency.com>
2022-08-13 10:18:43 -0500

    Add gameboard object to the files.

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+
+(function GameBoard() {
+    this.gameGrid = [
+    '1A',
+    '1B',
+    '1C',
+    '2A',
+    '2B',
+    '2C',
+    '3A',
+    '3B',
+    '3C',
+  ]
+})();
+
+
+
+// function test(gameGrid) {
+//   gameGrid.forEach(square => {
+//     console.lot(square)
+//   });
+// }
+
