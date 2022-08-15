673014a8ca80d36d847b63cd545d5d3e194a4b85
J-Skarbek <john@snarkmediaagency.com>
2022-08-15 16:15:10 -0500

    Create module IIFE to create gameboard and add divs to DOM.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,22 @@
+const GameBoard = (function() {
+  this.gameGrid = [
+  '1A',
+  '1B',
+  '1C',
+  '2A',
+  '2B',
+  '2C',
+  '3A',
+  '3B',
+  '3C',
+  ];
+  this.tileCounter = 0;
+  this.gameContainer = document.querySelector('.game-container');
 
-
-(function GameBoard() {
-    this.gameGrid = [
-    '1A',
-    '1B',
-    '1C',
-    '2A',
-    '2B',
-    '2C',
-    '3A',
-    '3B',
-    '3C',
-  ]
+  return this.gameGrid.forEach(square => {
+    this.tileCounter++ 
+    const gameSquare = document.createElement('div')
+    this.gameContainer.appendChild(gameSquare)
+    gameSquare.classList.add(`game-square-${this.tileCounter}`) 
+  });
 })();
-
-
-
-// function test(gameGrid) {
-//   gameGrid.forEach(square => {
-//     console.lot(square)
-//   });
-// }
-
