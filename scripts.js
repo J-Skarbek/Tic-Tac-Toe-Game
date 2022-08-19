baffd701361358251920a4136931c64d9699e61d
J-Skarbek <john@snarkmediaagency.com>
2022-08-19 16:43:32 -0500

    Add function to test the click event on each square of the gameboard. Several tests to get the main event clicks to create a new player object.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,92 @@
 const playerNameInput = Array.from(document.querySelectorAll('.player-input'))
+const player1Btn = document.querySelector('.player-1')
+const player2Btn = document.querySelector('.player-2')
+const player1Test = document.querySelector('.player-1').addEventListener('click', collectInfo)
+const player2Test = document.querySelector('.player-2').addEventListener('click', playerFactory)
 
-playerNameInput.forEach(player => {
-  player.addEventListener('click', () => {
-    if (player.classList.contains('player-1')) {
-      let player1Name = prompt("What's your name?")
-      let player1Preference = prompt("Do you want to be X's or O's?")
-      const player1 = playerFactory(`${player1Name}`,`${player1Preference}`)
-      console.log(player1Preference)
-      console.log(player1) 
-    } else if (player.classList.contains('player-2')) {
-      let player2Name = prompt("What's your name?")
-      let player2Preference = prompt("Do you want to be X's or O's?")
-      const player2 = playerFactory(`${player2Name}`,`${player2Preference}`)
-      console.log(player2Preference)
-      console.log(player2)
-    }
-  })
-});
+// document.querySelector('.player-1').addEventListener('click', () => {
+//   let player1Name = prompt("What's your name?")
+//   let player1Preference = prompt("Do you want to be X's or O's?")
+//   const player1 = playerFactory(`${player1Name}`,`${player1Preference}`)
+//   console.log(player1, typeof player1)
+// })
+// const player2Input = document.querySelector('.player-2').addEventListener('click', () => {
+//   let player2Name = prompt("What's your name?")
+//   let player2Preference = prompt("Do you want to be X's or O's?")
+//   const player2 = playerFactory(`${player2Name}`,`${player2Preference}`)
+//   console.log(player2, typeof player2)
+//   return {
+//     player2
+//   }
+// })
+
+function collectInfo() {
+  let playerName = prompt("What's your name?")
+  // let playerName = getName
+  let playerPreference = prompt("Do you want to be X's or O's?")
+  // let playerPreference = getPlayerPreference
+  // const test = alert(`${playerName} --> ${playerPreference}`)
+  const factoryTest = () => {
+    console.log(`${playerName} --> ${playerPreference}`)
+  }
+  const newPlayer = factoryTest()
+
+  return {
+    newPlayer
+  }
+}
+
+
+const player2Input = document.querySelector('.player-2').addEventListener('click', () => {
+  let player2Name = prompt("What's your name?")
+  let player2Preference = prompt("Do you want to be X's or O's?")
+  const player2 = playerFactory(`${player2Name}`,`${player2Preference}`)
+  console.log(player2, typeof player2)
+  return {
+    player2
+  }
+})
+
+
+// const Listners = (function() {
+
+//   let player1Name
+//   let player1Preference
+//   let player1
+//   let player2Name
+//   let player2Preference
+//   let player2
+
+//   function listenForPlayers() {
+//     playerNameInput.forEach(player => {
+//       player.addEventListener('click', () => {
+//         if (player.classList.contains('player-1')) {
+//           player1Name = prompt("What's your name?")
+//           player1Preference = prompt("Do you want to be X's or O's?")
+//           player1 = playerFactory(`${player1Name}`,`${player1Preference}`)
+//           console.log(player1Preference)
+//           console.log(player1) 
+//         } else if (player.classList.contains('player-2')) {
+//           player2Name = prompt("What's your name?")
+//           player2Preference = prompt("Do you want to be X's or O's?")
+//           player2 = playerFactory(`${player2Name}`,`${player2Preference}`)
+//           console.log(player2Preference)
+//           console.log(player2)
+//         }
+//       })
+//     });
+//   }
+
+
+//   const assignPlayers = listenForPlayers()
+
+//   return {
+//     assignPlayers, player1, player2
+//   }
+
+// })();
+
+/* Create gameboard object as IIFE/module */
 
 const GameBoard = (function() {
   const gameGrid = [
@@ -38,38 +108,53 @@ const GameBoard = (function() {
       const gameSquare = document.createElement('div')
       gameContainer.appendChild(gameSquare)
       gameSquare.classList.add(`game-square-${tileCounter}`)
+      gameSquare.addEventListener('click', () => {
+        console.log('testing clicks')
+      })
       console.log(tileCounter)
       tileCounter++
     })
     console.table(gameGrid)
     console.log(typeof gameGrid)
   }
+
   const initDomElements = createDomElements(gameGrid)
   return {
-    initDomElements
+    initDomElements,
+    gameGrid,
   }
 })();
 
-function theTest() {
-  console.log(typeof GameBoard)
-  console.log(tileCounter)
-  console.log(gameGrid)
-  console.log(gameSquare)
-  console.log(gameBoard)
-}
+/* Factory function for player creation */
 
 function playerFactory(name, preference) {
+  const getPlayerInfo = () => {
+    console.log(name),
+    console.log(preference)
+  }
+
+  const add = (a, b) => a + b;
+  
   return {
-    name, preference
+    add, getPlayerInfo, name, preference
   }
 }
 
-// const player1 = playerFactory(`${player1Name}, ${player1Preference}`)
-// const player2 = playerFactory(`${player2Name}, ${player2Preference}`)
+/* ++===== Testing Function ======= */
 
-function playerTest() {
-  console.log(player1)
-  console.log(typeof player1)
-  console.log(player2)
-  console.log(typeof player2)
+function playerFactory(name, preference) {
+
+  
+  const getPlayerInfo = () => {
+    console.log(name),
+    console.log(preference)
+  }
+
+  const add = (a, b) => a + b;
+  
+  return {
+    add, getPlayerInfo, name, preference
+  }
 }
+
+const josh = playerFactory('josh', 'x')
